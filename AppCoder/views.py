43799f03d82d4ac271fd13959afbb7c6211d12e6
Farid Tanta <faridtanta06@gmail.com>
2023-03-20 15:50:55 -0500

    Primer commit

---
+++ AppCoder/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,HttpResponse
+from AppCoder.models import Curso
+
+def curso(self):
+    curso = Curso(nombre="Desarrollo web", camada="19881")
+    curso.save()
+    documentoDeTexto= f"-->Curso: {curso.nombre} Camada: {curso.camada}"
+        
+    return HttpResponse(documentoDeTexto)
